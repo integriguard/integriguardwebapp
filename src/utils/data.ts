57bd04ab489ed5d2473a015737a2199d785aaecc
IntegriGuard <integriguardapp@outlook.com>
2024-05-06 17:02:42 +0800

    Initial Commit

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,39 +1,41 @@
 const services = [
     {
-        title: "Social media marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        title: "Medicare & Manage Care Compliance",
+        description: "Concurrent Live Review of Techincal Requirements, Skilled Level of Care Criteria and Triple Check Meeting Prep",
+        // icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        // </svg>`,
+        icon: `<img src="/images/doctor-filling-application.jpg" alt="Doctor Image" class="mx-auto w-30 h-20">
+        </img>`,
     },
     {
-        title: "Amazon affiliate marketing",
-        description: "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        title: "PDPM Missed Oppurtunities",
+        description: "HIPPS Coding, MD Query, Primary Dx, BIMS, PHQ9, and GG",
+        icon: `<img src="/images/pdpm.jpg" alt="PDPM Image" class="w-30 h-20"> 
+        </img>`,
     },
     {
-        title: "Email marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        title: "Quality Measures Trigger Alerts",
+        description: "5 Star Rating, SNF QRP",
+        icon: `<img src="/images/5star.jpg" alt="5Star Image" class="w-30 h-20"> 
+        </img>`,
     },
     {
-        title: "Social media marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        title: "Regulatory Compliance",
+        description: "MDS Scheduling, MDS Data Entry, MDS Due Alert, CAAs & Care Planning and MDS Coding Accurary Audit",
+        icon: `<img src="/images/mds3.jpg" alt="MDS3 Image" class="w-30 h-20"> 
+        </img>`,
     },
     {
-        title: "Amazon affiliate marketing",
-        description: "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        title: "PBJ Staffing Accuracy",
+        description: "Missing Assessments and Discharge MDS Completion",
+        icon: `<img src="/images/PBJ.png" alt="PBJ Image" class="w-30 h-20"> 
+        </img>`,
     },
     {
-        title: "Email marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        title: "Other Services",
+        description: "ICD10 Assistance, Skilled Documentation Review ADR and Audit & Denial Management",
+        icon: `<img src="/images/audit-and-denial.jpg" alt="Audit Image" class="w-30 h-20"> 
+        </img>`,
     },
 ];
 
