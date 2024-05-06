1d099cb8fefb0906fb042314b106cde3ebd45925
IntegriGuard <integriguardapp@outlook.com>
2024-05-06 17:22:27 +0800

    Updated site base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: 'https://integriguard.github.io',
-  // base: 'my-repo',
+  base: 'integriguardwebapp',
 });
