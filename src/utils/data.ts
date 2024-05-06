2ded2948b6f09f90eef1a153f1334bb096e8c6de
IntegriGuard <integriguardapp@outlook.com>
2024-05-06 17:32:16 +0800

    Updated images

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -4,7 +4,7 @@ const services = [
         description: "Concurrent Live Review of Techincal Requirements, Skilled Level of Care Criteria and Triple Check Meeting Prep",
         // icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         // </svg>`,
-        icon: `<img src="/images/doctor-filling-application.jpg" alt="Doctor Image" class="mx-auto w-30 h-20">
+        icon: `<img src="./images/doctor-filling-application.jpg" alt="Doctor Image" class="mx-auto w-30 h-20">
         </img>`,
     },
     {
