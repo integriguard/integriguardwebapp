622ac309b105e934a831716c78cf7614ddfb331a
IntegriGuard <integriguardapp@outlook.com>
2024-06-10 21:58:12 +0800

    Updated to use carousel

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -4,19 +4,19 @@ const services = [
         description: "Concurrent Live Review of Techincal Requirements, Skilled Level of Care Criteria and Triple Check Meeting Prep",
         // icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         // </svg>`,
-        icon: `<img src="./images/doctor-filling-application.jpg" alt="Doctor Image" class="mx-auto w-30 h-20">
+        icon: `<img src="/images/doctor-filling-application.jpg" alt="Doctor Image" class="mx-auto w-30 h-20">
         </img>`,
     },
     {
         title: "PDPM Missed Oppurtunities",
         description: "HIPPS Coding, MD Query, Primary Dx, BIMS, PHQ9, and GG",
-        icon: `<img src="./images/pdpm.jpg" alt="PDPM Image" class="w-30 h-20"> 
+        icon: `<img src="/images/pdpm.jpg" alt="PDPM Image" class="w-30 h-20"> 
         </img>`,
     },
     {
         title: "Quality Measures Trigger Alerts",
         description: "5 Star Rating, SNF QRP",
-        icon: `<img src="./images/5star.jpg" alt="5Star Image" class="w-30 h-20"> 
+        icon: `<img src="/images/5star.jpg" alt="5Star Image" class="w-30 h-20"> 
         </img>`,
     },
     {
