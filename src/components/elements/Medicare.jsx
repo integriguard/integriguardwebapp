4b696e62995183afccb12d058452793ffadfe32b
IntegriGuard <integriguardapp@outlook.com>
2024-06-10 22:04:42 +0800

    Change color

--- src/components/elements/Medicare.jsx
+++ src/components/elements/Medicare.jsx
@@ -13,7 +13,7 @@ function Collapse() {
       {isOpen && (
         <div className="text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content rounded-md p-1">
             <div className="justify-center">
-           <p className="text-center text-gray-700 dark:text-gray-300 text-lg ">Concurrent Live Review of Technical Requirements, Skilled Level of Care Criteria and Triple Check Meeting Prep</p>
+           <p className="text-center text-gray-700 dark:text-gray-300 text-lg">Concurrent Live Review of Technical Requirements, Skilled Level of Care Criteria and Triple Check Meeting Prep</p>
           </div>
         </div>
       )}
@@ -21,7 +21,7 @@ function Collapse() {
         className="bg-primary text-white peer peer-checked:bg-secondary peer-checked:text-secondary-content rounded-full px-4 py-2"
         onClick={toggleCollapse}
       >
-        {isOpen ? 'Show Less' : 'Learn More'}
+        {isOpen ? 'Show Less' : 'Learn More'} 
       </button>      
     </div>
   );
